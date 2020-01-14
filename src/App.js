d5381e6b16af9eb79b02faca894838f3fe691e0b
Jonghyun <30713876+nmj21c@users.noreply.github.com>
2020-01-14 13:22:08 +0900

    onClick nomad
    onClick nomad

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import "./styles.css";
 
-export default function App() {
+const useClick = onClick => {
+  const element = useRef();
+
+  console.log("onClick", element.current);
+
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+
+  return element;
+};
+
+const App = () => {
+  const clickMe = e => {
+    console.log(e);
+  };
+
+  const title = useClick(clickMe);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
-}
+};
+
+export default App;
